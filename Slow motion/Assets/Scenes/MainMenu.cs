dfe299091c72aa920f8d1fb1232e88f370f60ff3
richzii <rihards.ziverts@gmail.com>
2020-05-06 22:33:37 +0300

    Main menu updated

--- Slow motion/Assets/Scenes/MainMenu.cs
+++ Slow motion/Assets/Scenes/MainMenu.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuScript : MonoBehaviour {
-
+public class MainMenu : MonoBehaviour
+{
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void QuitGame() {
+    public void ExitGame() {
         Application.Quit();
     }
 }
