3128eced49f7fac371c182b0bebf782da53ce1ad
richzii <rihards.ziverts@gmail.com>
2020-05-28 00:38:04 +0300

    Linear interpolation added

---
+++ Slow motion/Assets/Scripts/Interpolation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interpolation : MonoBehaviour
+{
+    Vector3 startingPosition;
+    Vector3 endingPosition;
+
+    float time;
+    public float flightTime;
+
+    void Start()
+    {
+        startingPosition = new Vector3(195, 22, 4);
+        endingPosition = new Vector3(5, 22, 170);
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        transform.position = Vector3.Lerp(startingPosition, endingPosition, time/flightTime);
+    }
+}
