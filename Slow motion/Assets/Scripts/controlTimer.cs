456dd423a90795767fe875301482fefce66ebcda
richzii <rihards.ziverts@gmail.com>
2020-05-28 02:37:23 +0300

    Some more updates

---
+++ Slow motion/Assets/Scripts/controlTimer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class controlTimer : MonoBehaviour
+{
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.name == "startRace")
+        {
+            setTimer(1);
+        }
+
+        if (hit.gameObject.name == "finishRace")
+        {
+            setTimer(0);
+        }
+    }
+
+    void setTimer(int t)
+    {
+        timer playerTimer = this.GetComponent<timer>();
+        playerTimer.startTimer = t;
+    }
+}
---
+++ Slow motion/Assets/Scripts/timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class timer : MonoBehaviour
+{
+    public GameObject Timer;
+    public float startTimer = 0;
+    private float time;
+
+    void Update()
+    {
+        if (startTimer > 0)
+        {
+            time += Time.deltaTime;
+
+            var minutes = time / 60; 
+            var seconds = time % 60;
+            var fraction = (time * 100) % 100;
+
+            Timer.GetComponent<Text>().text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+        }
+    }
+}
