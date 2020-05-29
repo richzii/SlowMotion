fabff47f52f91aaa95907a09d3e8d3fa4122ab35
richzii <rihards.ziverts@gmail.com>
2020-05-29 18:04:41 +0300

    Finish camera included

--- Slow motion/Assets/Scripts/Finishing.cs
+++ Slow motion/Assets/Scripts/Finishing.cs
@@ -5,12 +5,21 @@ using UnityEngine.UI;
 
 public class Finishing : MonoBehaviour
 {
+    public Camera normalCamera;
+    public Camera finishCamera;
+
     void OnCollisionEnter(Collision collision)
     {
         CarUserControl control = FindObjectOfType<CarUserControl>();
         if (collision.gameObject.name == "InvisibleFinishLine") {
             Debug.Log("Car has reached the finish");
-            control.enabled = false; 
+            control.enabled = false;
+            ShowFinish();
+        }
+
+        void ShowFinish() {
+            normalCamera.enabled = false;
+            finishCamera.enabled = true;
         }
     }
 }
